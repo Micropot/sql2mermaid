b964b70f55dabb0c6cf9d9f0d23365db768bc573
Arthur LAMARD <arthur.lamard@chu-brest.fr>
2025-01-29 11:46:20 +0100

    fix html

--- pyscript/generate_graph.py
+++ pyscript/generate_graph.py
@@ -4,7 +4,6 @@ from sql2mermaid.sql2mermaid import mermaid_generator
 
 def generate_graph(event):
     input_text = document.querySelector("#mermaid-code")
-    print("coucou")
     sql_query = input_text.value
     output_div = document.querySelector("#output")
     output_div.innerText = mermaid_generator(sql_query)
