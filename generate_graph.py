750d8cb31770135b94de8bdd1a8b337c210569ff
Micropot <108392315+Micropot@users.noreply.github.com>
2025-01-29 13:45:02 +0000

    Deploying to gh-pages from @ Micropot/sql2mermaid@7dfc9c3eaa5836084af1f6760eaab9e721c9a242 🚀

--- generate_graph.py
+++ generate_graph.py
@@ -2,8 +2,23 @@ from pyscript import document
 from sql2mermaid.sql2mermaid import mermaid_generator
 
 
+# def generate_graph(event):
+#     input_text = document.querySelector("#mermaid-code")
+#     sql_query = input_text.value
+#     output_div = document.querySelector("#output")
+#     output_div.innerText = mermaid_generator(sql_query)
 def generate_graph(event):
     input_text = document.querySelector("#mermaid-code")
     sql_query = input_text.value
     output_div = document.querySelector("#output")
-    output_div.innerText = mermaid_generator(sql_query)
+
+    # Effacer l'ancien contenu
+    output_div.innerHTML = ""
+
+    # Créer un nouvel élément pour le diagramme
+    mermaid_div = document.createElement("div")
+    mermaid_div.setAttribute("class", "mermaid")
+    mermaid_div.innerText = mermaid_generator(sql_query)
+
+    # Ajouter au conteneur
+    output_div.appendChild(mermaid_div)
